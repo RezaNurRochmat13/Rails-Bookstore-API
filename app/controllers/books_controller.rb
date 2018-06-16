31d8e62c5973127735682b196116a5f438863070
rezanur <rezanur@uii.ac.id>
2018-06-16 19:25:01 +0700

    modify cors and create new books api

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,4 +10,14 @@ class BooksController < ApplicationController
         render json: @book
     end
 
+    def createNewBooks
+        @book = Book.new(book_name: params[:book_name], book_writer: params[:book_writer], book_publisher: params[:book_publisher], book_description: params[:book_description])
+        
+        if @book.save
+            render json: @book, status: 200
+        else
+            render json: {error: 'Insert data failed'}, status: 400
+        end 
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'books' => 'books#allBooks'
   get 'books/:id' => 'books#singleBooks'
+  post 'books' => 'books#createNewBooks'
 end
